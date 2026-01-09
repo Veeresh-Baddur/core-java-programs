4509dd3fe6a0797a48114a1d3fb03c6c3fe34119
Veeresh-Baddur <baddurveeresh@gmail.com>
2026-01-09 12:17:19 +0530

    Add palindrome number program

---
+++ numbers/PalindromeNumber.java
@@ -0,0 +1,26 @@
+//Palindrome number is number reads forword and backword as same
+//Example 121, 212, 777
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
